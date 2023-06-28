eb253b436ca666c897092740312c7d65fcfde639
Your Name <you@example.com>
2023-06-28 13:17:16 -0600

    seccion 19

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Cubits'),
+            subtitle: const Text('Gestor de estado simple'),
+            trailing: const Icon(Icons.arrow_back_ios_rounded),
+            onTap: () => context.push('/cubits'),
+          ),
+          ListTile(
+            title: const Text('Bloc'),
+            subtitle: const Text('Gestor de estado complejo'),
+            trailing: const Icon(Icons.arrow_back_ios_rounded),
+            onTap: () => context.push('/bloc'),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            child: Divider(),
+          ),
+        ],
+      ),
+    );
+  }
+}
